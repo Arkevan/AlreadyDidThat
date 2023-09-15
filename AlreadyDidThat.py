8b73f7d1b8cf5993a71f9ef84284d603343ee5b7
Arkevan <subject148994@gmail.com>
2023-09-15 16:23:31 +0500

    Working Code

--- AlreadyDidThat.py
+++ AlreadyDidThat.py
@@ -1,3 +1,5 @@
+#use functions
+
 def convert_to_base_10(number, base):
     if base == 10:
         return number  # No conversion needed, return the number as-is
@@ -6,28 +8,46 @@ def convert_to_base_10(number, base):
         return base_10_number
     except ValueError:
         raise ValueError("Invalid input. Make sure the number is valid for the given base.")
+def decimal_to_base(number, base):
+    if not 2 <= base <= 36:
+        raise ValueError("Base must be between 2 and 36")
 
+    if number == 0:
+        return "0"  # Special case for zero
 
+    digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+    result = ""
+    negative = False
 
+    if number < 0:
+        negative = True
+        number = abs(number)
 
+    while number > 0:
+        remainder = number % base
+        result = digits[remainder] + result
+        number = number // base
 
-# print(octal)
+    if negative:
+        result = "-" + result
 
+    return result
 def sorter(num, order):
-    newnum = str(num)
+    
     count = 0
-    for x in newnum:
+    for x in num:
         if x == 0:
             count += 1
-    sortednum = ''.join(sorted(newnum, reverse=order))
-    znumstr = sortednum.zfill(count)
-    final = int(znumstr)
+        sortednum = ''.join(sorted(num, reverse=order))
+        znumstr = sortednum.zfill(count)
+        final = znumstr
+    
 
     return final
-
-
 def uniquer(arr):
+    if len(arr) == 1:
+        return True
     seen = set()
     for item in arr:
         if item in seen:
@@ -36,13 +56,20 @@ def uniquer(arr):
     return True
 
 
+
+
 def did(id, base):
-    based = str(convert_to_base_10(id, base))
+    based = str(id)
     k = len(based)
     x = sorter(based, True)
     y = sorter(based, False)
-    z = x - y
-    fz = str(z)
+    
+    if base != 10:
+      z = convert_to_base_10(x, base) - convert_to_base_10(y, base)
+      fz = str(decimal_to_base(z, 3))
+    elif base == 10:
+        z = int(x) - int(y)
+        fz = str(z)
     var = len(fz)
     if var < k:
         zval = '0'
@@ -58,27 +85,38 @@ def did(id, base):
 def already(id, base):
     arr = []
     newN = did(id, base)
+    
     arr.append(newN)
-    while len(arr) > 1 and uniquer(arr) == True:
+    count = 0
+    while uniquer(arr) == True:
         contour = did(newN, base)
         newN = contour
         arr.append(newN)
+        
+        count += 1
+        #for case of constant like 1211 in base 10
+        if arr[count] == arr[count -1 ]:
+            return 1
+        
+    #breaks out of loop so uniquer(arr) no longer true    
+    #for repeating patterns like 210022 in base 3
+    
+    count2 = 0
+    rep = arr.index(arr[count])
+    fin = count - rep
+    print(fin)
 
-    # if algorithm reaches a constant
-    if len(arr) == 1:
-        return 1
-
-    final = len(arr)
-
-
-    return final
-
+    return fin
+    
+    
+    
 
 con = already(1211, 10)
 print("1211 is: ", con)
 
 cont = already(210022, 3)
-print('210022 is: ', cont)
-print(convert_to_base_10(210022, 3))
+print(cont)
+#print(convert_to_base_10(210022, 3))
 
-print(already(575, 10))
+#print(already(575, 10))
+
